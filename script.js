8134fd57a751fecd829c570ee79a0e5db4dcee9b
Asif Azad <66885542+BRAINIAC2677@users.noreply.github.com>
2020-08-17 11:57:33 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-for (var i = 0; i< names.length; i++) {
-  var c = names[i][0];
-  if (c === 'J' || c === 'j') {
-    byeSpeaker.speak(names[i]);
+(function () {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  for (var i = 0; i< names.length; i++) {
+    var c = names[i][0];
+    if (c === 'J' || c === 'j') {
+      byeSpeaker.speak(names[i]);
+    }
+    else {
+      helloSpeaker.speak(names[i]);
+    }
   }
-  else {
-    helloSpeaker.speak(names[i]);
-  }
-}
+
+})();
